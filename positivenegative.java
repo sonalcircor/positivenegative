42507fafc089734a07b59fcbaef218d9004163a2
Sonal Patel <tina_sonal@yahoo.co.in>
2022-07-29 18:29:31 +0100

    posative negative

---
+++ positivenegative.java
@@ -0,0 +1,18 @@
+package ifelse_hw_posneg;
+
+public class positivenegative {
+    public static void main(String[] args) {
+        int num = 2;
+        if (num > 0){
+            System.out.println("num is positive");
+        }
+        else
+        {
+            System.out.println("num is negative");
+
+        }
+
+    }
+
+    }
+
